3f1e102aeb9c6074dd6090fae74e3613d8eea3c6
Anartz Nuin <anartz@neonplay.com>
2018-01-24 12:43:58 +0000

    First implementation of show model

--- models/show_model.py
+++ models/show_model.py
@@ -1,3 +1,10 @@
 from google.appengine.ext import ndb
+
+
 class ShowModel(ndb.Model):
-    pass
+    country_code = ndb.StringProperty(name='cc')
+    channel_id = ndb.StringProperty(name='ci')
+    start_time = ndb.DateTimeProperty(name='st')
+    end_time = ndb.DateTimeProperty(name='et')
+    show_name = ndb.StringProperty(name='sn', indexed=False)
+    show_info = ndb.StringProperty(name='si', indexed=False)
