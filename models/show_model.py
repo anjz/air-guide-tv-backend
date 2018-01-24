658287bdd85f0c1740a1b640ce2a88bec1676c01
Anartz Nuin <anartz@neonplay.com>
2018-01-24 15:03:10 +0000

    Show id added to show model

--- models/show_model.py
+++ models/show_model.py
@@ -4,6 +4,7 @@ from google.appengine.ext import ndb
 class ShowModel(ndb.Model):
     country_code = ndb.StringProperty(name='cc')
     channel_id = ndb.StringProperty(name='ci')
+    show_id = ndb.StringProperty(name='sid')
     start_time = ndb.DateTimeProperty(name='st')
     end_time = ndb.DateTimeProperty(name='et')
     show_name = ndb.StringProperty(name='sn', indexed=False)
