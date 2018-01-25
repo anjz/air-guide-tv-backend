5b9c0e1b17e8f3d18d8296f206213de2cb1ee7aa
Anartz Nuin <anartz@neonplay.com>
2018-01-25 16:29:00 +0000

    Added route and handler implementation for el pais cron scraping task

---
+++ handler/cron_scrap.py
@@ -0,0 +1,31 @@
+import logging
+
+import webapp2
+
+from service.scraping_service import ScrapingService
+
+
+class CronScrapHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
+
+    def scrap_el_pais(self):
+        """
+        Scraps El Pais for today, tomorrow and after_tomorrow.
+        """
+        from service.scrapers.el_pais_scraper import ElPaisScraper
+        el_pais_scraper = ElPaisScraper()
+        scraping_service = ScrapingService(el_pais_scraper)
+
+        import arrow
+        today = arrow.now('Europe/Madrid')
+        tomorrow = today.shift(days=1)
+        after_tomorrow = tomorrow.shift(days=1)
+
+        scraping_service.scrap_and_store_shows_for_dates(today, tomorrow, after_tomorrow)
+
+        logging.info('El Pais tv shows information stored')
+
+        self.response.write('done')
+
+
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import webapp2
 from google.appengine.ext import vendor
 
 from handler.on_air import OnAirHandler
+from handler.cron_scrap import CronScrapHandler
 
 vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
 debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
 
 app = webapp2.WSGIApplication([
-    webapp2.Route('<country_code:[aA-zZ]{2,3}>', handler=OnAirHandler, methods='POST')
+    webapp2.Route('<country_code:[aA-zZ]{2,3}>', handler=OnAirHandler, methods='POST'),
+    webapp2.Route('/cron/scrap/elpais', handler=CronScrapHandler, handler_method='scrap_el_pais', methods='GET')
 ], debug=debug)
