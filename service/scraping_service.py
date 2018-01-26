6ddc1372c1e197c36aa422df70a5258e53ad83a0
Anartz Nuin <anartz@neonplay.com>
2018-01-26 12:03:27 +0000

    Added scraping entity as parent key for show entities and fixed a bug batching ndb puts

--- service/scraping_service.py
+++ service/scraping_service.py
@@ -1,5 +1,8 @@
 from google.appengine.ext import ndb
 
+import arrow
+from model.scrap_model import ScrapModel
+
 
 class ScrapingService:
     __MAX_STORE_BATCH_SIZE = 500
@@ -8,18 +11,34 @@ class ScrapingService:
         self.__scraper = scraper
 
     def scrap_and_store_shows_for_dates(self, *args):
-        for date in args:
-            self._scrap_and_store_shows(date)
+        scrap_entity_id = ScrapModel.generate_id_for_new_entity()
+        scrap_entity = ScrapModel.get_by_id(scrap_entity_id)
 
-    def _scrap_and_store_shows(self, date):
+        if scrap_entity is not None:
+            # Scraping data for today already exists. Delete it.
+            # todo delete entity and all its childs.
+            pass
+
+        scrap_entity = ScrapModel(
+            id=scrap_entity_id,
+            scrap_date_time=arrow.utcnow().naive,
+            amount_dates_to_scrap=len(args),
+            amount_dates_scraped=0
+        )
+
+        for date in args:
+            self._scrap_and_store_shows(date, scrap_entity)
+
+    def _scrap_and_store_shows(self, date, scrap_info_entity):
         shows_to_persist = []
         current_batch_size = 0
         for show in self.__scraper.get_shows_for_date(date):
             if current_batch_size < self.__MAX_STORE_BATCH_SIZE:
+                show.parent = scrap_info_entity.key()
                 shows_to_persist.append(show)
                 current_batch_size += 1
-
-            # Store current batch
-            ndb.put_multi(shows_to_persist)
-            shows_to_persist = []
-            current_batch_size = 0
+            else:
+                # Store current batch
+                ndb.put_multi(shows_to_persist)
+                shows_to_persist = []
+                current_batch_size = 0
