126eab7b7617f197c32e7d6596109eba276f51e5
Anartz Nuin <anartz@neonplay.com>
2018-02-01 15:26:33 +0000

    Added Content-Type header to responses

--- handler/shows.py
+++ handler/shows.py
@@ -10,6 +10,7 @@ class OnAirHandler(PublicHandler):
         timezone = HelperService.get_shows_info_tz_for_country(country_code)
         shows = ShowsService.retrieve_on_air_shows_for_country(timezone)
 
+        self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps(shows))
 
 
@@ -18,4 +19,5 @@ class ChannelHandler(PublicHandler):
         timezone = HelperService.get_shows_info_tz_for_country(country_code)
         shows = ShowsService.retrieve_shows_for_channel(channel_id, timezone)
 
+        self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps(shows))
