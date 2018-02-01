49b6e50f67c0457dee086fef60e5c9a20971861b
Anartz Nuin <anartz@neonplay.com>
2018-02-01 14:28:33 +0000

    Added route for getting shows for a channel

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import webapp2
 from google.appengine.ext import vendor
 
-from handler.shows import OnAirHandler
+from handler.shows import OnAirHandler, ChannelHandler
 from handler.cron_scrap import CronScrapHandler
 
 vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
@@ -10,5 +10,6 @@ debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/<country_code:[aA-zZ]{2,3}>', handler=OnAirHandler, methods='GET'),
+    webapp2.Route('/<country_code:[aA-zZ]{2,3}>/<channel_id:\d{1,3}>', handler=ChannelHandler, methods='GET'),
     webapp2.Route('/cron/scrap/elpais', handler=CronScrapHandler, handler_method='scrap_el_pais', methods='GET')
 ], debug=debug)
